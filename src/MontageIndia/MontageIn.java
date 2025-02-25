f6db892fb08ff54ceb2e615a7034052fa07f2cbe
Nawaz19977 <nawaz.akhtar@metageeks.tech>
2025-02-25 10:02:21 +0530

    first commit

---
+++ src/MontageIndia/MontageIn.java
@@ -0,0 +1,94 @@
+package MontageIndia;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.Select;
+
+public class MontageIn {
+
+private static WebElement element;
+
+public static void main(String[] args) throws InterruptedException {
+		
+WebDriver driver = new ChromeDriver();
+
+driver.manage().window().maximize();
+
+driver.get("https://www.montageindia.com/auth/user/login");
+
+Thread.sleep(1000);
+
+driver.findElement(By.xpath("//button[normalize-space()='Continue with Email']")).click();
+
+Thread.sleep(1000);
+
+driver.findElement(By.xpath("//input[@placeholder='Enter your email']")).sendKeys("teeesst@gmail.com");
+
+Thread.sleep(3000);
+
+driver.findElement(By.xpath("//input[@placeholder='Enter your password']")).sendKeys("Test@123456");
+
+Thread.sleep(4000);
+
+driver.findElement(By.xpath("/html/body/div/div[2]/div[2]/div[2]/div/button\r\n"+ "")).click();
+
+Thread.sleep(4000);
+
+//driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[2]/div[3]/div[2]/button[2]")).click();
+
+Thread.sleep(5000);
+
+//driver.findElement(By.xpath("//a[@href='/video']")).click();
+
+driver.findElement(By.xpath("//a[@href='/image']")).click();
+
+Thread.sleep(5000);
+
+driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[3]/div[2]/div/div[3]/div/div/div/div/div[2]/div[1]/div/div[1]/div/img")).click();
+
+Thread.sleep(5000);
+
+//driver.findElement(By.xpath("//input[@placeholder='Find your perfect stock']")).sendKeys("Images");
+
+driver.findElement(By.xpath("/html/body/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div/div[3]")).click();
+
+Thread.sleep(5000);
+
+driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div/div[2]/button")).click();
+
+Thread.sleep(5000);
+
+driver.findElement(By.xpath("//*[@id=\"cart-popup\"]/div[2]/a")).click();
+
+Thread.sleep(4000);
+
+//driver.findElement(By.xpath("//*[@id=\"state-select\"]")).click();
+WebElement stateDropdown = driver.findElement(By.id("state-select"));
+Select select = new Select(stateDropdown);
+select.selectByVisibleText("Assam");
+
+Thread.sleep(3000);
+
+driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[2]/table/tfoot/tr/td[5]/div/button[2]")).click();
+
+Thread.sleep(2000);
+
+driver.findElement(By.xpath("/html/body/div[8]/div/div[6]/button[1]")).click();
+
+Thread.sleep(2000);
+
+driver.findElement(By.xpath("/html/body/div/div[1]/div/div[3]/div[2]/div/div/div/form/div[1]/div[2]/div[2]/label/input")).sendKeys("7070537979");
+
+
+
+Thread.sleep(2000);
+
+}
+
+}
---
+++ src/MontageIndia/SeleniumScript.java
@@ -0,0 +1,94 @@
+package MontageIndia;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SeleniumScript {
+
+public static void main(String[] args) throws InterruptedException {
+		
+WebDriver driver = new ChromeDriver();
+
+driver.get("https://www.montageindia.com/auth/admin/login");
+
+Thread.sleep(1000);
+
+System.out.println(driver.getTitle());
+
+driver.manage().window().maximize();
+
+driver.findElement(By.id("usernameOrEmail")).sendKeys("nawaz123");
+
+Thread.sleep(3000);
+
+driver.findElement(By.id("password")).sendKeys("nawaz1234");
+
+Thread.sleep(3000);
+
+driver.findElement(By.xpath("/html/body/div/div[2]/div[2]/div/button")).click();
+
+System.out.println(driver.getTitle());
+
+Thread.sleep(3000);
+
+driver.findElement(By.xpath("//*[@id=\"r\"]/div/div[1]/ul/li[2]/a/span")).click();
+
+Thread.sleep(3000);
+
+driver.findElement(By.xpath("//a[normalize-space()='Create']")).click();
+
+Thread.sleep(3000);
+
+driver.findElement(By.xpath("/html/body/div/div[2]/div[1]/div/div[1]/ul/li[2]/ul/li[4]/a")).click();
+
+Thread.sleep(3000);
+
+driver.findElement(By.xpath("/html/body/div/div[2]/div[1]/div/div[1]/ul/li[2]/ul/li[5]/a")).click();
+
+Thread.sleep(3000);
+
+driver.findElement(By.xpath("/html/body/div/div[2]/div[1]/div/div[1]/ul/li[2]/ul/li[6]/a")).click();
+
+Thread.sleep(3000);
+
+driver.findElement(By.xpath("//*[@id=\"r\"]/div/div[1]/ul/li[3]/a/span")).click();
+
+Thread.sleep(3000);
+
+/*driver.findElement(By.xpath("/html/body/div/div[2]/div[1]/div/div[1]/ul/li[3]/ul/li[2]/a")).click();
+
+Thread.sleep(3000);
+
+driver.findElement(By.xpath("/html/body/div/div[2]/div[1]/div/div[1]/ul/li[3]/ul/li[3]/a")).click();
+
+Thread.sleep(3000);
+
+driver.findElement(By.xpath("/html/body/div/div[2]/div[1]/div/div[1]/ul/li[4]/ul/li[1]/a")).click();
+
+Thread.sleep(3000);
+
+driver.findElement(By.xpath("/html/body/div/div[2]/div[1]/div/div[1]/ul/li[4]/ul/li[2]/a")).click();
+
+Thread.sleep(3000);
+
+driver.findElement(By.xpath("/html/body/div/div[2]/div[1]/div/div[1]/ul/li[4]/ul/li[3]/a")).click();
+
+Thread.sleep(3000);
+
+driver.findElement(By.xpath("/html/body/div/div[2]/div[1]/div/div[1]/ul/li[4]/ul/li[4]/a")).click();
+
+Thread.sleep(3000);
+
+driver.findElement(By.xpath("/html/body/div/div[2]/div[1]/div/div[1]/ul/li[4]/ul/li[5]/a")).click();*/
+
+
+
+}
+
+}
